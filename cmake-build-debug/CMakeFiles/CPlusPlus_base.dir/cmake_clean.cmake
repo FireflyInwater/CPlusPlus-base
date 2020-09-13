5e84df0605b8d0977597659fa5989e8aaa056325
xbchen <1026377981@qq.com>
2020-09-13 23:31:12 +0800

    new c++prj

---
+++ cmake-build-debug/CMakeFiles/CPlusPlus_base.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPlusPlus_base.dir/main.cpp.o"
+  "CPlusPlus_base"
+  "CPlusPlus_base.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CPlusPlus_base.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
